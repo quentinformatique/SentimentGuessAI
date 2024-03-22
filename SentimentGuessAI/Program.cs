b4c3631b3f0b1d0b0d3b6e302ec16b4f4d698f45
quentinformatique <quentin161204@gmail.com>
2024-03-22 21:20:14 +0100

    use model

--- SentimentGuessAI/Program.cs
+++ SentimentGuessAI/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//charger les données
+using SentimentGuessAI;
+
+var sampleData = new MLModel1.ModelInput()
+{
+    Col0 = "C# is such an amazing language !",
+};
+
+// charger le pmodele et prédire
+var result = MLModel1.Predict(sampleData);
+
+// Si la prédiction est 1, le sentiment est "Positif"; sinon, le sentiment est "Negatif"
+var sentiment = result.PredictedLabel == 1 ? "Positif" : "Negatif";
+Console.WriteLine($"Texte: {sampleData.Col0}\nSentiment: {sentiment}");
