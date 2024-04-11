9aae4db812e0e180cf59006cedf36fd0e19134ee
quentinformatique <quentin161204@gmail.com>
2024-04-11 21:28:44 +0200

    add comments

--- SentimentGuessAI/Program.cs
+++ SentimentGuessAI/Program.cs
@@ -3,6 +3,7 @@ using SentimentGuessAI;
 
 var sampleData = new MLModel1.ModelInput()
 {
+    // la phrase a évaluer (elle doit etre en anglais
     Col0 = "C# is such an amazing language !",
 };
 
@@ -11,4 +12,5 @@ var result = MLModel1.Predict(sampleData);
 
 // Si la prédiction est 1, le sentiment est "Positif"; sinon, le sentiment est "Negatif"
 var sentiment = result.PredictedLabel == 1 ? "Positif" : "Negatif";
+// on affiche la prédiction
 Console.WriteLine($"Texte: {sampleData.Col0}\nSentiment: {sentiment}");
